9e342bc5339aa5295181fef634f3f2f2d7b7b5ff
marcos012 <marcospaulo.ad@gmail.com>
2018-01-07 18:19:34 -0200

    Utilizando Vue Material

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png">-->
     <!-- aqui aparece o conteudo a ser exibido -->
-    <router-view/>
+    <!-- <router-view/> -->
+    <md-card>
+      <md-card-header>
+        <div class="md-title">Titulo</div>
+        <div class="md-title">Subtitulo</div>
+      </md-card-header>
+      <md-card-content>
+        <md-button class="md-raised md-primary" @click="table()">Table</md-button>
+        <md-button class="md-raised md-primary" @click="limpar()">Limpar</md-button>
+      </md-card-content>
+    </md-card>
+
+    <md-table>
+      <md-table-row>
+        <md-table-head md-numeric>ID</md-table-head>
+        <md-table-head>Name</md-table-head>
+        <md-table-head>Email</md-table-head>
+        <md-table-head>Gender</md-table-head>
+        <md-table-head>Job Title</md-table-head>
+      </md-table-row>
+
+      <md-table-row>
+        <md-table-cell md-numeric>1</md-table-cell>
+        <md-table-cell>Shawna Dubbin</md-table-cell>
+        <md-table-cell>sdubbin0@geocities.com</md-table-cell>
+        <md-table-cell>Male</md-table-cell>
+        <md-table-cell>Assistant Media Planner</md-table-cell>
+      </md-table-row>
+
+      <md-table-row>
+        <md-table-cell md-numeric>2</md-table-cell>
+        <md-table-cell>Odette Demageard</md-table-cell>
+        <md-table-cell>odemageard1@spotify.com</md-table-cell>
+        <md-table-cell>Female</md-table-cell>
+        <md-table-cell>Account Coordinator</md-table-cell>
+      </md-table-row>
+
+      <md-table-row>
+        <md-table-cell md-numeric>3</md-table-cell>
+        <md-table-cell>Vera Taleworth</md-table-cell>
+        <md-table-cell>vtaleworth2@google.ca</md-table-cell>
+        <md-table-cell>Male</md-table-cell>
+        <md-table-cell>Community Outreach Specialist</md-table-cell>
+      </md-table-row>
+    </md-table>
   </div>
 </template>
 
